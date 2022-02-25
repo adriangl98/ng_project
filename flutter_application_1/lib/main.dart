594f1b9b15fb494ef2f651d55f29583ffb705a16
Adrian <hudaku@tamu.edu>
2022-02-24 23:45:35 -0600

    colo,title

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -4,9 +4,17 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("I am Rich")),
-        backgroundColor: Colors.red[900],
-      ),
+          body: Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://thecornerbcs.com/wp-content/uploads/2019/03/bg-top-1.png'),
+            ),
+          ),
+          backgroundColor: Color.fromARGB(255, 158, 19, 9),
+          appBar: AppBar(
+            title: Text("I am Rich"),
+            backgroundColor: Colors.red[900],
+          )),
     ),
   );
 }
